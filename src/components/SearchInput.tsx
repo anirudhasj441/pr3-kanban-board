de696c00eb6f93f161e606e238708f528d70b99c
anirudha.jadhav <anirudhasj441@gmail.com>
2024-03-05 12:21:40 +0530

    fix: search input label floating mechanism

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -3,22 +3,80 @@ import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 
 const SearchInput: React.FC = () => {
     return (
-        <div className="seerch-container group mb-3 rounded-md border-none outline focus-within:outline-blue-500 outline-1 relative flex gap-0 items-center pe-2">
-            <label
-                htmlFor="search-input"
-                className="absolute px-2 ease-in-out duration-500 group-focus-within:text-blue-500 top-1/2 -translate-y-1/2 group-focus-within:-top-0 group-focus-within:translate-y-0 transition-all group-focus-within:text-[10px]"
-            >
-                Search Project
-            </label>
+        <div className="
+            seerch-container
+            mb-3
+            group
+
+            rounded-md
+            border-none
+
+            outline
+            outline-blue-500
+
+            has-[input:placeholder-shown]:outline-slate-500
+            has-[input:focus]:outline-blue-500
+
+            outline-1
+
+            relative
+
+            flex
+            gap-0
+            items-center
+
+            pe-2
+        ">
             <input
                 type="text"
                 id="search-input"
-                className="p-2 w-full border-none outline-none flex-1 bg-transparent"
+                className="
+                    peer
+                    p-2
+                    w-full
+                    border-none
+                    outline-none
+                    flex-1
+                    bg-t
+                    ransparent
+                    placeholder:text-transparent
+                "
+                placeholder="seacrh"
             />
+            <label
+                htmlFor="search-input"
+                className="
+                    absolute
+                    px-2
+                    ease-in-out
+                    duration-500
+                    transition-all
+                    bg-white
+                    z-9
+
+                    top-0
+                    left-2
+                    text-[12px]
+                    text-blue-500
+                    -translate-y-1/2
+
+                    peer-focus:top-0
+                    peer-focus:left-2
+                    peer-focus:text-[12px]
+                    peer-focus:text-blue-500
+
+                    peer-placeholder-shown:top-1/2
+                    peer-placeholder-shown:left-0
+                    peer-placeholder-shown:text-slate-500
+                    peer-placeholder-shown:text-base
+                "
+            >
+                Search Project
+            </label>
             <MagnifyingGlassIcon
                 height="30"
                 width={30}
-                className="group-focus-within:text-blue-500"
+                className="peer-focus:text-blue-500 text-slate-500"
             />
         </div>
     );
