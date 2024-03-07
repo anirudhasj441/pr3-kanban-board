ffe0e221cb016262aa6b4532220fb11a58a80a5a
Anirudha Jadhav <anirudhasj441@gmail.com>
2024-03-07 21:01:56 +0530

    fix: remove temp code

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,27 +1,12 @@
 import React from "react";
 import programmerImg from "../assets/programmer.png";
-import { Button } from "@radix-ui/themes";
-import { PlusIcon, MinusIcon } from "@radix-ui/react-icons";
-import { counterStore } from "../stores";
 
 const StartPage: React.FC = () => {
-    const count = counterStore((state) => state.count);
-    const countIncrement = counterStore((state) => state.increment);
-    const countDecrement = counterStore((state) => state.decrement);
     return (
         <div className="h-full flex justify-center items-center">
             <div className="flex flex-col items-center w-[40%] opacity-[.7] gap-2">
                 <img src={programmerImg} className="" alt="" />
                 <div className="text-2xl">Coding Devil</div>
-                <div className="flex gap-3">
-                    <Button onClick={countDecrement}>
-                        <MinusIcon />
-                    </Button>
-                    <div className="text-2xl">{count}</div>
-                    <Button onClick={countIncrement}>
-                        <PlusIcon />
-                    </Button>
-                </div>
             </div>
         </div>
     );
