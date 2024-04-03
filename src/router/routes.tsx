2629cae4d6f96a78b45de5e62eab41ba16487ef8
Anirudha Jadhav <anirudhasj441@gmail.com>
2024-04-03 22:00:44 +0530

    dev: Add loading skeleton for project page

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,7 +1,11 @@
 import { RouteObject } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import StartPage from "../pages/StartPage";
-import ProjectPage from "../pages/ProjectPage";
+import React, { Suspense } from "react";
+import ProjectPageSkeleton from "../skeletons/ProjectPage";
+
+// eslint-disable-next-line react-refresh/only-export-components
+const ProjectPage = React.lazy(() => import("../pages/ProjectPage"));
 
 const routes: RouteObject[] = [
     {
@@ -14,7 +18,11 @@ const routes: RouteObject[] = [
             },
             {
                 path: ":project_id",
-                element: <ProjectPage />,
+                element: (
+                    <Suspense fallback={<ProjectPageSkeleton />}>
+                        <ProjectPage />
+                    </Suspense>
+                ),
             },
         ],
     },
---
+++ src/skeletons/ProjectPage.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import MyScrollArea from "../components/ScrollArea";
+
+const ProjectPageSkeleton: React.FC = () => {
+    return (
+        <div className="h-full flex gap-5">
+            {[...Array(3)].map(() => (
+                <div className="flex-1 flex flex-col gap-5">
+                    <div>
+                        <Skeleton
+                            // enableAnimation={false}
+                            className="py-3 rounded-md m-0"
+                            count={1}
+                        />
+                    </div>
+                    <MyScrollArea className="flex-grow">
+                        <Skeleton
+                            // enableAnimation={false}
+                            inline={true}
+                            className="py-3 rounded-md m-0"
+                            containerClassName="flex flex-col h-full gap-2"
+                            count={8}
+                        />
+                    </MyScrollArea>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default ProjectPageSkeleton;
