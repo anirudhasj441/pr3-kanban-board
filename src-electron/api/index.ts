25d23dae67c89e0752278d9ff882732e6651d0de
anirudha.jadhav <anirudhasj441@gmail.com>
2024-04-16 10:06:25 +0530

    clean: specified return types of electronApi preload methods

--- src-electron/api/index.ts
+++ src-electron/api/index.ts
@@ -12,47 +12,56 @@ const electronAPI = {
     maximize: (): void => {
         ipcRenderer.send("maximize");
     },
-    createProject: (projectTitle: string): void => {
-        ipcRenderer.send("createProject", projectTitle);
-    },
-    createTask: (projectId: string, taskTitle: string, status: Status) => {
-        ipcRenderer.send("createTask", projectId, taskTitle, status);
-    },
     getProjects: (): void => {
         ipcRenderer.send("getProjects");
     },
     getTasks: (projectId: string): void => {
         ipcRenderer.send("getTasks", projectId);
     },
-    deleteTask: (projectId: string, taskId: string) => {
+    createProject: (projectTitle: string): void => {
+        ipcRenderer.send("createProject", projectTitle);
+    },
+    createTask: (
+        projectId: string,
+        taskTitle: string,
+        status: Status
+    ): void => {
+        ipcRenderer.send("createTask", projectId, taskTitle, status);
+    },
+    deleteTask: (projectId: string, taskId: string): void => {
         ipcRenderer.send("deleteTask", projectId, taskId);
     },
-    deleteProject: (projectId: string) => {
+    deleteProject: (projectId: string): void => {
         ipcRenderer.send("deleteProject", projectId);
     },
-    projectExists: (projectId: string) => {
+    projectExists: (projectId: string): void => {
         ipcRenderer.send("projectExists", projectId);
     },
-    updateTaskDesc: (projectId: string, taskId: string, desc: string) => {
+    updateTaskDesc: (projectId: string, taskId: string, desc: string): void => {
         ipcRenderer.send("updateTaskDesc", projectId, taskId, desc);
     },
-    updateTaskName: (projectId: string, taskId: string, taskName: string) => {
+    updateTaskName: (
+        projectId: string,
+        taskId: string,
+        taskName: string
+    ): void => {
         ipcRenderer.send("updateTaskName", projectId, taskId, taskName);
     },
     updateTaskProgress: (
         projectId: string,
         taskId: string,
         progress: number
-    ) => {
+    ): void => {
         ipcRenderer.send("updateTaskProgress", projectId, taskId, progress);
     },
-    updateAllTasks: (projectId: string, tasks: Task[]) => {
+    updateAllTasks: (projectId: string, tasks: Task[]): void => {
         ipcRenderer.send("updateAllTasks", projectId, tasks);
     },
     receive: (channel: string, func: (...args: any) => void) => {
         ipcRenderer.on(
             channel,
-            (_event: Electron.IpcRendererEvent, ...args: any) => func(...args)
+            (_event: Electron.IpcRendererEvent, ...args: any): void =>
+                func(...args)
         );
     },
 };
