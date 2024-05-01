a960e09c79db1af28c3057150b2883d42b9f97b4
Anirudha Jadhav <anirudhasj441@gmail.com>
2024-05-01 22:01:07 +0530

    fix: add props for decide show remove btn or not

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -5,26 +5,33 @@ import { TagColor } from "../../../types";
 interface TagProps {
     label: string;
     color: TagColor;
+    showRemoveBtn?: boolean;
 }
 
 const Tag: React.FC<TagProps> = (props: TagProps) => {
     props.label;
     return (
         <div
-            className="tag rounded-full px-2 py-1 flex gap-1 items-center text-white font-bold"
+            className="tag rounded-full px-2 py-1 flex gap-1 items-center text-white font-bold w-max"
             style={{ backgroundColor: props.color }}
         >
             <div className="text-xs">{props.label}</div>
-            <button>
-                <Icon
-                    icon="material-symbols:close"
-                    width={15}
-                    height={15}
-                    className="block"
-                />
-            </button>
+            {props.showRemoveBtn ? (
+                <button>
+                    <Icon
+                        icon="material-symbols:close"
+                        width={15}
+                        height={15}
+                        className="block"
+                    />
+                </button>
+            ) : null}
         </div>
     );
+};
+
+Tag.defaultProps = {
+    showRemoveBtn: false,
 };
 
 export default Tag;
