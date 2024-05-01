fb1790cb0310ceaa6a86c785e8ff5ded6a570e60
Anirudha Jadhav <anirudhasj441@gmail.com>
2024-05-01 22:02:20 +0530

    dev: create dialog for add tag form

--- src/components/TaskDetail.tsx
+++ src/components/TaskDetail.tsx
@@ -6,7 +6,8 @@ import Editor from "./Editor";
 import { EditorState } from "lexical";
 import MySlider from "./Slider";
 import Seperator from "./Seperator";
-// import Tag from "./Tag";
+import * as Dialog from "@radix-ui/react-dialog";
+import CreateTagForm from "./Tag/CreateTagForm";
 
 interface TaskDetailProps {
     task: Task;
@@ -16,7 +17,7 @@ interface TaskDetailProps {
 
 const TaskDetail: React.FC<TaskDetailProps> = (props: TaskDetailProps) => {
     const [editMode, setEditMode] = useState<boolean>(false);
-
+    const [createTagForm, setCreateTagForm] = useState<boolean>(false);
     const onSave = useCallback(
         (editorState: EditorState | undefined) => {
             props.onUpdateDesc && props.onUpdateDesc(editorState);
@@ -72,15 +73,67 @@ const TaskDetail: React.FC<TaskDetailProps> = (props: TaskDetailProps) => {
                 <div className="">
                     <div className="text-slate-600">TAGS</div>
                     <div className="flex gap-2 items-center flex-wrap">
-                        <button className="bg-gray-600 text-white text-xs px-2 py-1 rounded-full flex gap-1 items-center hover:bg-gray-500 active:bg-gray-700">
-                            <Icon
-                                icon="material-symbols:add"
-                                className="block"
-                                width={15}
-                                height={15}
+                        <Dialog.Root
+                            open={createTagForm}
+                            onOpenChange={setCreateTagForm}
+                        >
+                            <Dialog.Trigger asChild>
+                                <button className="bg-gray-600 text-white text-xs px-2 py-1 rounded-full flex gap-1 items-center hover:bg-gray-500 active:bg-gray-700">
+                                    <Icon
+                                        icon="material-symbols:add"
+                                        className="block"
+                                        width={15}
+                                        height={15}
+                                    />
+                                    <div className="">Add tag</div>
+                                </button>
+                            </Dialog.Trigger>
+                            <Dialog.Overlay
+                                className="fixed backdrop-blur-sm transition-all duration-900 ease-linear z-10"
+                                style={{ inset: 0 }}
                             />
-                            <div className="">Add tag</div>
-                        </button>
+                            <Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-100 z-20 rounded-md w-[300px] shadow-lg cursor-auto flex flex-col">
+                                <div className="h-full flex flex-col p-4">
+                                    {/* <Dialog.Title className="flex">
+                                        <div className="text-2xl text-slate-600">
+                                            Add tag
+                                        </div>
+                                        <Space></Space>
+
+                                        <button
+                                            className="hover:bg-gray-300 p-1 rounded-md transition duration-800 ease-in-out"
+                                            onClick={() =>
+                                                setCreateTagForm(false)
+                                            }
+                                        >
+                                            <Icon
+                                                icon="material-symbols:close"
+                                                width={25}
+                                                height={25}
+                                                className="block text-slate-600"
+                                            />
+                                        </button>
+                                    </Dialog.Title> */}
+                                    {/* <Seperator /> */}
+                                    <CreateTagForm />
+                                    <Seperator className="mt-3" />
+                                    <ul className="tag-list py-2">
+                                        <li className="flex gap-1 hover:bg-gray-300 p-2 active:bg-gray-400 rounded-md">
+                                            <Icon
+                                                icon="material-symbols:circle"
+                                                className="block"
+                                                width={25}
+                                                height={25}
+                                                style={{
+                                                    color: "red",
+                                                }}
+                                            />
+                                            <div className="text-md">bug</div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </Dialog.Content>
+                        </Dialog.Root>
                     </div>
                 </div>
             </div>
